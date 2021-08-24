bf340859f86dcf12c2fafc14e4b47260b8fdb53a
Akib <Akib@DESKTOP-8SFKD39>
2021-08-24 13:22:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to University of South Florida");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
