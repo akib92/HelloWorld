f82d01f45b39e5b2196b61216f55a852affa9cd6
Akib <Akib@DESKTOP-8SFKD39>
2021-08-24 13:27:13 -0700

    Version 1.1-- HelloWorld

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Welocme to University of South Florida");
             Console.WriteLine("Hello World!");
             Console.ReadKey(true);
+            Console.WriteLine("Press any key to continue and close the program");
         }
     }
 }
